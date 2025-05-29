33677fe70ac856f0d1adb3cf2ab4d526eaa8183f
Jena-Biswajit <biswajitjena529@gmail.com>
2025-05-29 11:50:28 +0530

    factory design pattern modified

--- design-pattern/src/main/java/com/example/factory/factorymodel/VehicleFactory.java
+++ design-pattern/src/main/java/com/example/factory/factorymodel/VehicleFactory.java
@@ -7,15 +7,15 @@ import com.example.factory.models.Truck;
 
 public class VehicleFactory {
     public static Vehicle getVehicle(String vehicleType){
-        if (vehicleType.equals("Car")){
-            return new Car();
-        } else if (vehicleType.equals("Truck")) {
-            return new Truck();
-        } else if (vehicleType.equals("Bike")) {
-            return new Bike();
-        }
-        else{
-            throw new IllegalArgumentException("unknown vehicle type");
-        }
+       switch (vehicleType){
+           case "Car":
+               return new Car();
+           case "Truck":
+               return new Truck();
+           case "Bike":
+               return new Bike();
+           default:
+               throw new IllegalArgumentException("unknown vehicle type");
+       }
     }
 }
