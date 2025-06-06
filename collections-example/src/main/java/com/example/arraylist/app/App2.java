a71303822a5f8ea5220e3b00739e87b649325f05
Jena-Biswajit <biswajitjena529@gmail.com>
2025-06-06 14:19:46 +0530

    list example

---
+++ collections-example/src/main/java/com/example/arraylist/app/App2.java
@@ -0,0 +1,4 @@
+package com.example.arraylist.app;
+
+public class App2 {
+}
---
+++ collections-example/src/main/java/com/example/arraylist/models/Employee.java
@@ -0,0 +1,22 @@
+package com.example.arraylist.models;
+
+public class Employee {
+    public int id ;
+    public String name ;
+    public String city ;
+
+    public Employee(int id , String name , String city ){
+        this.id = id ;
+        this.name = name ;
+        this.city = city ;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
